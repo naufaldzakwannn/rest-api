65649207dd2cd4fc9389b3bf279f3f9be0769e73
Naufal189 <naufaldzakwan189@gmail.com>
2025-09-16 16:35:32 +0700

    Fixed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,11 +9,12 @@ use App\Http\Middleware\VerifyCsrfToken;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        //  
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
